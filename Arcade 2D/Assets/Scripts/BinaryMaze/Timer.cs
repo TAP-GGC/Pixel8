994016919e5cf117f302a378e1e4d6ebfe48d2cf
Shianne <slesure1@ggc.edu>
2024-09-22 22:15:26 -0400

    Timer is partially working.
    starting before the player moves

---
+++ Arcade 2D/Assets/Scripts/BinaryMaze/Timer.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Timer : MonoBehaviour
+{
+    bool stopWatchActive = false;
+    float currentTime;
+    public Text currentTimeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = 0;
+    }
+
+    /*
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(stopWatchActive == true)
+        {
+            currentTime = currentTime + Time.deltaTime;
+        }
+        currentTime = currentTime + Time.deltaTime;
+        TimeSpan time = TimeSpan.FromSeconds(currentTime);
+        currentTimeText.text = time.ToString(@"mm\:ss");
+    }
+    */
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            stopWatchActive = true;
+            //currentTime = currentTime + Time.deltaTime;
+            TimeSpan time = TimeSpan.FromSeconds(currentTime);
+            currentTimeText.text = time.ToString(@"mm\:ss");
+        }
+    }
+}
