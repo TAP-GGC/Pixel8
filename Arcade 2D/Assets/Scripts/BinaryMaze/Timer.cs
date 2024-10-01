caa15a1f45b59e1f4d8947e93adb8fac596d1a7c
Shianne <slesure1@ggc.edu>
2024-10-01 13:33:41 -0400

    able to exit through time limit

--- Arcade 2D/Assets/Scripts/BinaryMaze/Timer.cs
+++ Arcade 2D/Assets/Scripts/BinaryMaze/Timer.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.SceneManagement;
 
 public class Timer : MonoBehaviour
 {
@@ -20,27 +21,22 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        /*
-        if(stopWatchActive == true)
-        {
-            currentTime = currentTime + Time.deltaTime;
-        }
-        */
         currentTime = currentTime + Time.deltaTime;
         TimeSpan time = TimeSpan.FromSeconds(currentTime);
         currentTimeText.text = time.ToString(@"mm\:ss");
-    }
-    
-     /*
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if(collision.CompareTag("Player"))
-        {
-            stopWatchActive = true;
-            currentTime = currentTime + Time.deltaTime;
-            TimeSpan time = TimeSpan.FromSeconds(currentTime);
-            currentTimeText.text = time.ToString(@"mm\:ss");
+        /*
+        if (currentTimeText.text.Equals("00:30")){
+            Invoke("GameOver", 30);
         }
+        */
+
+        Invoke("GameOver", 30);
     }
-    */
+
+    void GameOver()
+    {
+        // will make Binary exit button transition to the main menu
+        SceneManager.LoadSceneAsync(0);
+    }
+
 }
