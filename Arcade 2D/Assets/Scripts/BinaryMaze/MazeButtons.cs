1c9dd73e7db1174d12bebb8911d3a493251e3885
Shianne <slesure1@ggc.edu>
2024-10-13 22:53:02 -0400

    Made map smaller and a toggle

--- Arcade 2D/Assets/Scripts/BinaryMaze/MazeButtons.cs
+++ Arcade 2D/Assets/Scripts/BinaryMaze/MazeButtons.cs
@@ -6,8 +6,10 @@ using UnityEngine.SceneManagement;
 public class MazeButtons : MonoBehaviour
 {
     public Enabled_Disabled hint;
+    public Enabled_Disabled map;
     public Timer timer;
     private bool isHintActive = false; // Boolean to track the state
+    private bool isMapActive = false;
 
     void Start()
     {
@@ -17,6 +19,12 @@ public class MazeButtons : MonoBehaviour
             hint.DisableHint();
         }
         timer = GameObject.FindObjectOfType<Timer>();
+
+        map = GameObject.FindGameObjectWithTag("map").GetComponent<Enabled_Disabled>();
+        if (map != null)
+        {
+           map.DisableMiniMap();
+        }
     }
 
     public void exitBinaryMaze()
@@ -57,6 +65,19 @@ public class MazeButtons : MonoBehaviour
                 timer.StartTime();
             }
             hint.DisableHint(); // Disable hint
+        }
+    }
+
+    public void ToggleMap()
+    {
+        isMapActive = !isMapActive;
+        if (isMapActive)
+        {
+            map.EnableMiniMap();
+        }
+        else
+        {
+            map.DisableMiniMap();
         }
     }
 }
