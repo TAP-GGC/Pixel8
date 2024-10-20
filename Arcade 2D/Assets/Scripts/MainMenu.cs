fea3c9dfab0fc04f01ba6b50495dcaa67083ebe4
Zuberi Thompson <zthompson5@ggc.edu>
2024-10-20 11:22:49 -0400

    Pushing Drawing title card

--- Arcade 2D/Assets/Scripts/MainMenu.cs
+++ Arcade 2D/Assets/Scripts/MainMenu.cs
@@ -47,7 +47,7 @@ public class MainMenu : MonoBehaviour
     {
         if (hasPlayedBinary || hasPlayedPlanet)
         {
-            SceneManager.LoadSceneAsync(3);
+            SceneManager.LoadScene("Drawing Title");
         }
         
         else if (warningText != null)
