c7a780bba761d111199adbd6a40e759ecf20f69e
Celeste Echols <cechols@ggc.edu>
2024-11-16 12:36:52 -0500

    S3 update
    Added disable button and fixed score

--- Arcade 2D/Assets/Scripts/Planet 01000010/Answers.cs
+++ Arcade 2D/Assets/Scripts/Planet 01000010/Answers.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Answers : MonoBehaviour
 {
@@ -11,11 +12,26 @@ public class Answers : MonoBehaviour
     public GameObject correctText;
     public GameObject wrongText;
 
+    public static Button choice1;
+    public static Button choice2;
+    public static Button choice3;
+    public static Button choice4;
+
     public Game game;
     public void Start()
     {
+        isCorrect = false;
         correctText.SetActive(false);
         wrongText.SetActive(false);
+        choice1 = GameObject.Find("Choice1").GetComponent<Button>();
+        choice2 = GameObject.Find("Choice2").GetComponent<Button>();
+        choice3 = GameObject.Find("Choice3").GetComponent<Button>();
+        choice4 = GameObject.Find("Choice4").GetComponent<Button>();
+
+        choice1.interactable = false;
+        choice2.interactable = false;
+        choice3.interactable = false;
+        choice4.interactable = false;
     }
     public void Answer()
     {
@@ -24,6 +40,10 @@ public class Answers : MonoBehaviour
             if (isCorrect)
             {
                 Debug.Log("Correct Answer");
+                Answers.choice1.interactable = false;
+                Answers.choice2.interactable = false;
+                Answers.choice3.interactable = false;
+                Answers.choice4.interactable = false;
                 StartCoroutine(DisplayC_TextAfterDelay(.2f));
                 Game.MovePlayer(1);
                 StartCoroutine(RemoveC_TextAfterDelay(2f));
@@ -32,6 +52,10 @@ public class Answers : MonoBehaviour
             else
             {
                 Debug.Log("Wrong Answer");
+                Answers.choice1.interactable = false;
+                Answers.choice2.interactable = false;
+                Answers.choice3.interactable = false;
+                Answers.choice4.interactable = false;
                 StartCoroutine(DisplayW_TextAfterDelay(.2f));
                 StartCoroutine(RemoveW_TextAfterDelay(2f));
                 game.player1MoveText.gameObject.SetActive(false);
